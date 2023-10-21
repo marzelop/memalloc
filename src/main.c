07f1d302ad6afd7eb29947f93985eccc9938e6c4
marzelop <maagyo.teo23@gmail.com>
2023-10-20 23:34:33 -0300

    Bug bizarro novamente

--- src/main.c
+++ src/main.c
@@ -2,8 +2,6 @@
 
 #define PTR_NUM 10
 
-
-
 int blockAvailable(void *p) {
 	return ((long*) p)[-2] == 0;
 }
@@ -17,10 +15,12 @@ void printHeapBlockInfo(void *p) {
 }
 
 int main() {
-	void *p[PTR_NUM], *q, *r, *nb;
+	void *p[PTR_NUM], *q, *r, *nb, *s, *big, *t;
 	int i;
 	setup_brk();
-	printf("brk: %p\n", brkv);
+	// brki = brkv;
+	printf("brk inicial: %p\n", brkv);
+	printf("Alocando um vetor de ponteiros, onde cada ponteiro aponta para um bloco de tamanho 10(i+1), onde i é o índice do ponteiro no vetor.\n");
 	p[0] = memory_alloc(10);
 	for (i = 1; i < PTR_NUM; i++) {
 		p[i] = memory_alloc(10*(i + 1));
@@ -54,7 +54,38 @@ int main() {
 	nb = r + 50 + 16;
 	printf("Novo bloco:\n");
 	printHeapBlockInfo(nb);
-	// printf("O novo bloco vazio deve conter %lu bytes. Que seria %lu - 50 - 16 ((tamanho do bloco antigo completo) - (tamanho do novo bloco utilizado) - (tamanho do registro do bloco)).\n%s\n\n", 10*i - 50 - 16, 10*i, getBlockSize(nb) == 10*i - 50 - 16 ? "Correto" : "Errado");
+	printf("O novo bloco vazio deve conter %u bytes. Que seria %u - 50 - 16 ((tamanho do bloco antigo completo) - (tamanho do novo bloco utilizado) - (tamanho do registro do bloco)).\n%s\n", 10*i - 50 - 16, 10*i, getBlockSize(nb) == 10*i - 50 - 16 ? "Correto" : "Errado");
+	printf("O novo bloco não deve estar ocupado. %s\n\n", blockAvailable(nb) ? "Correto" : "Errado");
+
+	printf("Liberando p[%d].\n", PTR_NUM-2);
+	memory_free(p[PTR_NUM-2]);
+	printf("Alocando 30 bytes. Que cabem no último bloco livre e no penúltimo bloco livre.\n");
+	s = memory_alloc(30);
+	printHeapBlockInfo(s);
+	printf("Bloco deve estar alocado na mesma posição de p[%d] (penúltimo bloco livre/first-fit). %s\n\n", PTR_NUM-2, 
+	s == p[PTR_NUM-2] ? "Correto" : "Errado");
+	
+	// Libera toda a memória alocada dinamicamente (q, r, s estão inclusos no vetor p e nb é um bloco livre)
+	printf("Liberando toda a memória utilizada no momento.\n");
+	for (i = 0; i < PTR_NUM - 2; i++) {
+		memory_free(p[i]);
+	}
+	
+	printf("Alocando 1GB.\n");
+	big = memory_alloc(1*1024*1024*1024); // 1GB
+	printHeapBlockInfo(big);
+	printf("Bloco deve ter 1GB de tamanho. %s\n", getBlockSize(big) == 1*1024*1024*1024 ? "Correto" : "Errado");
+	printf("Liberando 1GB.\n\n");
+	memory_free(big);
+
+	printf("Alocando 200 bytes.\n");
+	t = memory_alloc(200);
+	printHeapBlockInfo(t);
+	printf("O bloco está depois de p[%d]? %s\n", PTR_NUM-1, t > p[PTR_NUM-1] ? "Sim, isso acontece porque o alocador não junta dois blocos vazios em sequência, ou seja, os blocos podem diminuir de tamanho, mas nunca aumentar." : "NÃO? Algo deve ter dado muito errado.\n");
+	printf("brk atual: %p\nRestaurando brk.\n", brkv);
+	dismiss_brk(brkv);
+	// printf("brk atual: %p\nbrk inicial: %p\nAmbos devem ser iguais. %s\n", brkv, brki, brkv == brki ? "Correto" : "Errado");
+
 
 	return 0;
 }
